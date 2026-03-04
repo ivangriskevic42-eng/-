9f38bd70f2787e2108e1d88e651b113bf817c290
ivangriskevic42-eng <ivangriskevic42@gmail.com>
2026-03-04 20:03:26 +0300

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int i=0;
+        int a=7;
+        System.out.print("Result:");
+        while(i<14){
+            i++;
+            System.out.print(i*a+" ");
+
+        }
+
+    }
+}
